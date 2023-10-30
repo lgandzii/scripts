ad0742a568f932923631c76480eb915ba7c7d4cb
LeoG <lgandzii@gmail.com>
2023-10-30 12:01:13 -0400

    new dir

--- minikube_install.sh
+++ minikube_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo su
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 sudo echo deb http://apt.kubernetes.io/ kubernetes-xenial main > /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update 
